f7c5a809e441fc41cd243bc43a27d8316444c8a0
Marco Mameli <mameli.1.marco@gmail.com>
2022-01-14 10:22:49 +0100

    Update
    Added metrics withouth labels
    Added predict return
    Changing in the configuration testing for the new function testing and added necessary calling for the

--- machine_learning/clustering.py
+++ machine_learning/clustering.py
@@ -24,7 +24,8 @@ def training_kmeans(config, data):
                     init="k-means++",
                     max_iter=config['model']['max_iter'],
                     n_init=1,
-                    verbose=config['model']['verbose'])
+                    verbose=config['model']['verbose'],
+                    algorithm="full")
 
     km.fit(data)
 
@@ -39,14 +40,17 @@ def test_kmeans(confg, model, data, labels):
     """
 
     from data.preprocessing.text_features_extraction import load_vectorized
-
+    # with label
     homogeneity_score = metrics.homogeneity_score(labels, model.labels_)
     completeness_score = metrics.completeness_score(labels, model.labels_)
     v_measure = metrics.v_measure_score(labels, model.labels_)
     adjusted_rand_index = metrics.adjusted_rand_score(labels, model.labels_)
-    silhouette_coefficient = metrics.silhouette_score(data, model.labels_, sample_size=1000)
+    # no label
+    silhouette_coefficient = metrics.silhouette_score(data, model.labels_, sample_size=config['model']['test_sample_size'])
+    davies_bouldin = metrics.davies_bouldin_score(data.toarray(), model.labels_)
 
-    return homogeneity_score, completeness_score, v_measure, adjusted_rand_index, silhouette_coefficient
+
+    return homogeneity_score, completeness_score, v_measure, adjusted_rand_index, silhouette_coefficient, davies_bouldin
 
 def save_model(config, model):
     """
@@ -80,30 +84,32 @@ def infer_kmeans(config, input):
     :return:
     """
 
+    # load model
     model = load_model(config)
+    # predict
+    predict = model.predict(input)
 
-    # Model inference
-
-    return model
+    return predict
 
 if __name__ == "__main__":
-    from data.preprocessing.text_features_extraction import extract_features_from_text, load_vectorized
+    from data.preprocessing.text_features_extraction import extract_features_from_text
     import pandas as pd
     config = {
         "model": {
-            "init_size": 1000,
+            "init_size": 100,
             "batch_size": 10,
             "verbose": True,
-            "n_clusters": 2,
+            "n_clusters": 4,
             "max_iter": 10,
-            "minibatch": False,
+            "minibatch": True,
+            "test_sample_size": 100
         },
         "api_configuration": {
             "model_path": "saved_data/model/cluster.sav",
             "extractor_path": "saved_data/vectorizer/vectorizer.sav"
         },
         "features": {
-            "n_features": 250,
+            "n_features": 500,
             "reduce": False,
             "reduced_n_features": 100,
 
@@ -119,6 +125,17 @@ if __name__ == "__main__":
     save_model(config, km)
     model = load_model(config)
 
-    print(test_kmeans(config, model, features, [random.randint(0, 1) for i in range(0, 5)]))
+    print(test_kmeans(config, model, features, [random.randint(0, config['model']['n_clusters'] - 1) for i in range(0, 5)]))
 
     print(model)
+
+    input_data = dataframe.loc[975]
+
+    input_features = extract_features_from_text(config, input_data, ['occupation_preferred_label', 'occupation_description',
+                                                              'isco_preferred_label', 'isco_group_description',
+                                                              'occupation_skill_skill_type'], "english", True,
+                                          "hashing")
+
+    prediction = infer_kmeans(config, input_features)
+
+    print(prediction)
