792f20e3dfba433fbfa16b9cafe577ff083ea0da
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-03 11:58:57 +0100

    Inserted the db uses for the aggregator

--- api/training.py
+++ api/training.py
@@ -1,9 +1,11 @@
 from flask_restful import Resource
 from machine_learning.clustering import training_kmeans, save_model
 from data.preprocessing.text_features_extraction import extract_features_from_text
+from data.data_aggragator import get_aggregated_dataframe
 import pandas as pd
 ml_config = {}
 dataset_path = ""
+db_connector = None
 
 def set_config(config):
     global ml_config
@@ -13,6 +15,11 @@ def set_dataset_path(path):
     global dataset_path
     dataset_path = path
 
+def set_db(db):
+    global db_connector
+    db_connector = db
+
+
 class Training(Resource):
     def get(self, source):
         response = {"status": 0,
@@ -21,7 +28,7 @@ class Training(Resource):
             if source == "file":
                 dataframe = pd.read_csv(dataset_path)
             elif source == "database":
-                dataframe = "null" #TODO change to SQL data
+                dataframe = get_aggregated_dataframe(db_connector)
             features = extract_features_from_text(ml_config, dataframe,
                                                   ['occupation_preferred_label', 'occupation_description',
                                                    'isco_preferred_label', 'isco_group_description',
