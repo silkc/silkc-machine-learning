95d8049285edc52071b2a77ec1918a584d6c483b
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-03 14:54:41 +0100

    added the connection to the database

--- api/api.py
+++ api/api.py
@@ -3,9 +3,10 @@ from flask_restful import Api
 import api.training as training
 import api.inference as inference
 
-def start_api(ml_config, dataframe_path=None, debug=True):
+def start_api(ml_config, db_connector, dataframe_path=None, debug=True):
     training.set_config(ml_config)
     training.set_dataset_path(dataframe_path)
+    training.set_db(db_connector)
     app = Flask(__name__)
     api = Api(app)
 
