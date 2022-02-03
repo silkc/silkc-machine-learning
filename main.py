202130dd81ee407f780f1cfa4293cbefd0f0f6aa
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-03 10:18:50 +0100

    Inserted the api activation parameters
    adding the start_api function

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import pandas as pd
 from data.preprocessing.text_features_extraction import extract_features_from_text
 from machine_learning.clustering import training_kmeans, save_model
 
+from api.api import start_api
+
 args = argparse.ArgumentParser()
 args.add_argument('--database_configuration', '-dbc', type=str, help="The path to the configuration of the database connection", required=False)
 args.add_argument('--machine_learning_configuration', '-mlc', type=str, help="The path to the configuration of the machine learning")
 args.add_argument('--train', help="Activate the training for the machine learning model", required=False, action='store_true')
 args.add_argument('--dataset', help="Get the dataset from file", type=str, required=False)
+args.add_argument('--api', help="Activate the api", required=False, action="store_true")
 parsed = args.parse_args()
 
 #%% get configuration
@@ -41,3 +44,8 @@ if parsed.train:
 
     km = training_kmeans(ml_config, features)
     save_model(ml_config, km)
+elif parsed.api:
+    if parsed.dataset is not None:
+        start_api(ml_config=ml_config, dataframe_path=parsed.dataset)
+    else:
+        start_api(ml_config=ml_config)
