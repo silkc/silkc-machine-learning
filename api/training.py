8c1d6740f31b2363dc5ec2d3242653bd8492280a
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-03 10:51:20 +0100

    Adding some check
    TODO create the response correctly

--- api/training.py
+++ api/training.py
@@ -1,4 +1,3 @@
-from flask import request
 from flask_restful import Resource
 from machine_learning.clustering import training_kmeans, save_model
 from data.preprocessing.text_features_extraction import extract_features_from_text
@@ -17,16 +16,19 @@ def set_dataset_path(path):
 class Training(Resource):
     def get(self, source):
         response = {}
-        if source == "file":
-            dataframe = pd.read_csv(dataset_path)
-        elif source == "database":
-            dataframe = "null" #TODO change to SQL data
-        features = extract_features_from_text(ml_config, dataframe,
-                                              ['occupation_preferred_label', 'occupation_description',
-                                               'isco_preferred_label', 'isco_group_description',
-                                               'occupation_skill_skill_type'], "english", True,
-                                              "hashing")
+        if source in ['file', 'database']:
+            if source == "file":
+                dataframe = pd.read_csv(dataset_path)
+            elif source == "database":
+                dataframe = "null" #TODO change to SQL data
+            features = extract_features_from_text(ml_config, dataframe,
+                                                  ['occupation_preferred_label', 'occupation_description',
+                                                   'isco_preferred_label', 'isco_group_description',
+                                                   'occupation_skill_skill_type'], "english", True,
+                                                  "hashing")
 
-        km = training_kmeans(ml_config, features)
-        save_model(ml_config, km)
+            km = training_kmeans(ml_config, features)
+            save_model(ml_config, km)
+        else:
+            response = {"no"} #TODO change the response
         return {"ciao"}
