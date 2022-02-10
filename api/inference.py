b372c4d1e13d63e2b67d0ff3363bdb7996cc2199
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-10 20:04:36 +0100

    Adding response message and one error segnaling

--- api/inference.py
+++ api/inference.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 from flask import request
 from machine_learning.classification import infer_classifier
 import pandas as pd
+import os
 
 configuration = {}
 db_connector = None
@@ -23,15 +24,27 @@ class Inference(Resource):
         response = {
             "status": 0,
             "response": {
-                "classification": 0,
-                "accuracy": 0,
+                "message": "",
+                "result": 0,
+                "accuracy": 0.0,
                 "inference_time": 0
             }
         }
         
         data = request.get_json(force=True)
-        dataframe = pd.DataFrame(data['input'], index=[0])
-        inference = infer_classifier(config=configuration, input=dataframe)
-        response['status'] = 200
-        response['response']['classification'] = int(inference[0])
+        keys = configuration['api']['inference']['input_key']
+        if all(key in data['input'] for key in keys):
+            # Opening the classification report:
+            report = pd.read_json(os.path.join(configuration['save_path']['report']['base_path'], configuration['save_path']['report']['report_name']))
+            
+            dataframe = pd.DataFrame(data['input'], index=[0]) #TODO use the mapping for the relation_type
+            inference, i_time = infer_classifier(config=configuration, input=dataframe)
+            response['status'] = 200
+            response['response']['result'] = int(inference[0])
+            response['response']['message'] = "Classification model executed"
+            response['response']['accuracy'] = report['precision']['accuracy']
+            response['response']['inference_time'] = i_time
+        else:
+            response['status'] = 406
+            response['response']['message'] = "The keys in the request does not respect what is expected from the ML algorithm"
         return response
