e8a89056ab8673273b89ff7438bd2f30a0005e0b
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-16 11:19:29 +0100

    Updates to the classification models
    This update uses the configuration for saving the different models singularly
    TODO give information about the number of NaN deleted from the dataset used

--- machine_learning/classification.py
+++ machine_learning/classification.py
@@ -21,12 +21,14 @@ def train_classifier(model_config:dict, save_config:dict, data:pd.DataFrame):
         config ([type]): [description]
         data ([type]): [description]
     """
-    classifier = DecisionTreeClassifier(min_samples_split=model_config['classificator']['min_samples_split'], random_state=model_config['classificator']['random_state'])
+    classifier = DecisionTreeClassifier(min_samples_split=model_config['train']['min_samples_split'], random_state=model_config['train']['random_state'])
     
     data.dropna(subset=model_config['input_columns'], how="any", inplace=True)
     data.to_csv('./dataframe.csv')
     
-    train, test = train_test_split(data, test_size=model_config['split_percentage'])
+    # TODO insert the number of data that was deleted from the dataframe 
+    
+    train, test = train_test_split(data, test_size=model_config['train']['split_percentage'])
     
     train_features = train[model_config['input_columns']]
     train_features.is_copy = False
@@ -38,18 +40,18 @@ def train_classifier(model_config:dict, save_config:dict, data:pd.DataFrame):
     
     classifier.fit(train_features, train_target)
     
-    save_model(save_config, classifier)
+    save_model(save_config['model']['base_path'], model_config['name'], classifier)
     
     prediction, report = test_classifier(classifier, test_features, test_target)
     
-    os.makedirs(save_config['report']['base_path'], exist_ok=True)
+    os.makedirs(os.path.join(save_config['report']['base_path'], model_config['name'].split('.')[0]) , exist_ok=True)
     
-    classification_report_csv(report=report, save_path=os.path.join(save_config['report']['base_path'], save_config['report']['report_name']))
+    save_classification_report(report=report, save_path=os.path.join(save_config['report']['base_path'], model_config['name'].split('.')[0], save_config['report']['classification']['textual']))
     #confusion_matrix_plot(test_target, prediction, os.path.join(save_config['report']['base_path'], save_config['report']['cf_name']))
     
     return classifier
 
-def classification_report_csv(report, save_path):
+def save_classification_report(report, save_path):
     df = pd.DataFrame(report).transpose()
     
     df.to_json(save_path)
@@ -84,7 +86,7 @@ def confusion_matrix_plot(target, predicted, save_path):
     #plt.show()
     plt.savefig(save_path)
 
-def save_model(config, model):
+def save_model(save_path, save_name, model):
     """
 
     :param config:
@@ -92,25 +94,24 @@ def save_model(config, model):
     :return:
     """
     import os
-    save_path = os.path.join(config['model'], config['model_name'])
-    os.makedirs(config['model'], exist_ok=True)
-    pickle.dump(model, open(save_path, 'wb'))
+    os.makedirs(save_path, exist_ok=True)
+    pickle.dump(model, open(os.path.join(save_path, save_name), 'wb'))
     return None
 
-def load_model(config):
+def load_model(save_path:str, model_name: str):
     """
 
     :param config:
     :return:
     """
 
-    model = pickle.load(open(os.path.join(config['save_path']['model'], config['save_path']['model_name']), 'rb'))
+    model = pickle.load(open(os.path.join(save_path, model_name), 'rb'))
 
     return model
 
-def infer_classifier(config, input):
+def infer_classifier(save_config, model_config, input):
     
-    model = load_model(config=config)
+    model = load_model(save_config['model']['base_path'], model_config['name'])
     
     start_time = time.time()
     
