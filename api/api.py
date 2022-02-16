1e0a0e0976c61b331faedb462ffc656d8aef8768
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-16 11:24:57 +0100

    Updates to the API URI

--- api/api.py
+++ api/api.py
@@ -3,16 +3,16 @@ from flask_restful import Api
 import api.training as training
 import api.inference as inference
 
-def start_api(config, db_connector, dataframe_path=None, debug=True):
+def start_api(config, db_connector, dataframes_path=None, debug=True):
     training.set_config(config)
-    training.set_dataset_path(dataframe_path)
+    training.set_datasets_path(dataframes_path)
     training.set_db(db_connector)
     inference.set_config(config=config)
     inference.set_db(db_connector)
     app = Flask(__name__)
     api = Api(app)
 
-    api.add_resource(training.Training, f"/{config['api']['training']['uri']}/<string:source>")
-    api.add_resource(inference.Inference, f"/{config['api']['inference']['uri']}")
+    api.add_resource(training.Training, f"/{config['api']['training']['base_uri']}/<string:model>/<string:source>")
+    api.add_resource(inference.Inference, f"/{config['api']['inference']['base_uri']}")
 
     app.run(debug=debug)
