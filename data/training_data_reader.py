6c0c861acb0be04327f666f695de44dc6770bd75
Marco Mameli <mameli.1.marco@gmail.com>
2022-02-21 09:42:39 +0100

    Changed the training id return
    from dictionary to list

--- data/training_data_reader.py
+++ data/training_data_reader.py
@@ -1,7 +1,7 @@
 import mysql.connector as mysql_connector
 import pandas as pd
 
-import sql_reader
+from data import sql_reader
 
 def get_training_list(db: mysql_connector.CMySQLConnection, table:str = 'training', column_list:str = '*') -> list:
     return sql_reader.read_sql_data_to_list(db, table, column_list)
@@ -9,8 +9,8 @@ def get_training_list(db: mysql_connector.CMySQLConnection, table:str = 'trainin
 def get_training_dataframe(db: mysql_connector.CMySQLConnection, table:str = 'training', column_list:str = '*') -> pd.DataFrame:
     return sql_reader.read_sql_data_to_dataframe(db, table, column_list)
 
-def get_training_ids(db: mysql_connector.CMySQLConnection, table:str = 'trraining', column: str = 'id') -> pd.DataFrame:
-    return get_training_dataframe(db, table, column_list=column)
+def get_training_ids(db: mysql_connector.CMySQLConnection, table:str = 'training', column: str = 'id') -> pd.DataFrame:
+    return get_training_list(db, table, column_list=column)
 
 if __name__ == "__main__":
     db = sql_reader.connect_to_database('localhost', 'root', 'root', 'SILCK', 33061)
