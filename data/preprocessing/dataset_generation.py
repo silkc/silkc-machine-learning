d2e2fab003d6fcbe99d013db8616cbccd2e96636
Marco Mameli <mameli.1.marco@gmail.com>
2022-06-16 20:16:18 +0200

    Adding total dataset generation

--- data/preprocessing/dataset_generation.py
+++ data/preprocessing/dataset_generation.py
@@ -12,3 +12,16 @@ def adding_columns(model_config:dict, dataframe:pd.DataFrame, columns_name:list)
         for id in id_list:
             dataframe.at[index, id] = 1
     return dataframe
+
+def total_adding_columns(dataframe:pd.DataFrame, target_column_name:str, columns_name:list) -> pd.DataFrame:
+    columns_list = dataframe.columns.tolist()
+    columns_list.remove(target_column_name)
+    
+    dataframe_rows_number = len(dataframe.index)
+    for name in columns_name:
+        dataframe[name] = [0] * dataframe_rows_number
+    for index, row in dataframe.iterrows():
+        id_list = row[target_column_name].split(',')
+        for id in id_list:
+            dataframe.at[index, id] = 1
+    return dataframe
